4fa44a8db410fbe74694751323cc7dc35fe104d3
shubham12008 <80684205+shubham12008@users.noreply.github.com>
2022-01-09 20:58:09 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -2,7 +2,7 @@ class Food {
     constructor () {
       
         this.foodStock = 0;
-        this.image = loadImage("Milk.png");
+        this.image = loadImage("Images/milkImage.png");
         this.lastFed;
     }
 
